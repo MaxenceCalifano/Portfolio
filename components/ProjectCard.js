7452a98b73b883f6694cc4d434ad77fb72d78913
Maxence <maxence.califano@gmail.com>
2022-06-30 15:49:25 +0200

    add all from previous repo

---
+++ components/ProjectCard.js
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import styles from '../styles/projectCard.module.css'
+import { useContext } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { ThemeContext } from "./Themes/ThemeContext";
+
+export default function ProjectCard(props) {
+    const height = props.height;
+    const width = props.width;
+
+    const {theme} = useContext(ThemeContext)
+    return (
+        <>
+        {props.reverse ?
+
+<div className={theme ==='dark' ? `${styles.card} ${styles.reverseCard}` : `${styles.card} ${styles.card_light} ${styles.reverseCard}`}>
+        
+
+<div className={`${styles.image} ${styles.imageReverse}`}>
+   <a href={props.demoLink}>
+        <Image 
+        src={props.imgSrc}
+        width={width}
+        height={height}
+        layout='responsive'
+        sizes="50vw"
+        alt={`capture d'écran du projet ${props.title}`}
+        className={styles.project_image}
+        > 
+        </Image>
+    </a>
+</div>         
+<div className={styles.content}>
+    <h4 className={styles.project_title}>{props.title}</h4>
+    <div className={`${styles.project_description} ${styles[props.projectNumber]} ${styles.project_descriptionReverse} `}>
+        <p>{props.children}</p>
+    </div>
+    <ul className={styles.project_tech_list}>
+        {props.tech_list.map(i => <li key={i}>{i} </li>)}
+    </ul>
+    <div className={styles.project_links}>
+        <a href={props.githubLink}><FontAwesomeIcon icon={faGithub} title="lien vers le github du projet"/></a>
+        <a href={props.demoLink}>Demo</a>
+    </div>
+</div>
+</div>
+        
+        : <div className={theme ==='dark' ? `${styles.card}` : `${styles.card} ${styles.card_light} `}>
+        
+        
+           <a className={styles.image} href={props.demoLink}>
+                <Image 
+                src={props.imgSrc}
+                width={width}
+                height={height}
+                layout='responsive'
+                sizes="50vw"
+                className={styles.project_image}
+                alt={`capture d'écran du projet ${props.title}`}
+                > 
+                </Image>
+            </a>
+        {/* </div>  */}    
+        <div className={styles.content}>
+            <h4 className={styles.project_title}>{props.title}</h4>
+            <div className={`${styles.project_description} ${styles[props.projectNumber]}`}>
+                <p>{props.children}</p>
+            </div>
+            <ul className={styles.project_tech_list}>
+                {props.tech_list.map( i => <li key={i}>{i} </li>)}
+            </ul>
+            <div className={styles.project_links}>
+                <a href={props.githubLink}><FontAwesomeIcon icon={faGithub} title="lien vers le github du projet"/></a>
+                <a href={props.demoLink}>Demo</a>
+            </div>
+        </div>
+    </div>}
+        </>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import ThemeContextWrapper from '../components/Themes/ThemeContextWrapper'
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContextWrapper>
+      <Component {...pageProps} />
+      </ThemeContextWrapper>
+  )
+  
 }
 
 export default MyApp
